c23c578707c199dd9155d7d61fd10cf4110526ed
trgcghai <conghai.tpma@gmail.com>
2026-01-10 00:07:27 +0700

    refactor: simplify Home component by removing unused queries and redirecting to /hub/fyp

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,91 +1,13 @@
 "use client";
-import {
-  FeaturedEmployers,
-  FeaturedJobs,
-  HeroSection,
-  LatestBlogs
-} from "@/app/(main)/components";
-import { useFetchAvailableBlogsQuery } from "@/services/blog/blogApi";
-import { useFetchJobsAvailableQuery } from "@/services/job/jobApi";
-import { useFetchAvailableRecruitersQuery } from "@/services/recruiter/recruiterApi";
-import { useCheckRecruitersFollowedQuery } from "@/services/user/userApi";
-import { useMemo } from "react";
-import { useUser } from "@/hooks/useUser";
-import { useCheckSavedJobsQuery } from "@/services/user/savedJobsApi";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const { user, isSignedIn } = useUser();
-  const {
-    data: blogsData,
-    isLoading: isLoadingBlogs,
-    isError: isErrorBlogs
-  } = useFetchAvailableBlogsQuery({
-    page: 1,
-    size: 3
-  });
-  const {
-    data: jobsData,
-    isLoading: isLoadingJobs,
-    isError: isErrorJobs
-  } = useFetchJobsAvailableQuery({
-    page: 1,
-    size: 3
-  });
-  const {
-    data: recruitersData,
-    isLoading: isLoadingRecruiters,
-    isError: isErrorRecruiters
-  } = useFetchAvailableRecruitersQuery({ page: 1, size: 3 });
+  const router = useRouter();
 
-  const jobs = useMemo(() => jobsData?.data?.result || [], [jobsData]);
-  const recruiters = useMemo(() => recruitersData?.data?.result || [], [recruitersData]);
-  const blogs = useMemo(() => blogsData?.data?.result || [], [blogsData]);
+  useEffect(() => {
+    router.push("/hub/fyp")
+  }, [router])
 
-  const { data: checkSavedJobsData } =
-    useCheckSavedJobsQuery(
-      {
-        jobIds: jobs.map((job) => job.jobId)
-      },
-      {
-        skip: !jobs || jobs.length === 0 || !user || !isSignedIn
-      }
-    );
-
-  const { data: checkFollowedData } =
-    useCheckRecruitersFollowedQuery(
-      {
-        recruiterIds: recruiters.map((recruiter) => recruiter.accountId)
-      },
-      {
-        skip: !recruiters || recruiters.length === 0 || !isSignedIn || !user
-      }
-    );
-
-  const savedJobs = useMemo(() => checkSavedJobsData?.data || [], [checkSavedJobsData]);
-  const followedRecruiters = useMemo(() => checkFollowedData?.data || [], [checkFollowedData]);
-
-  console.log(user);
-
-  return (
-    <>
-      <HeroSection />
-      <FeaturedJobs
-        jobs={jobs}
-        isLoading={isLoadingJobs}
-        isError={isErrorJobs}
-        savedJobs={savedJobs}
-      />
-      <FeaturedEmployers
-        recruiters={recruiters}
-        isLoading={isLoadingRecruiters}
-        isError={isErrorRecruiters}
-        followedRecruiters={followedRecruiters}
-      />
-      <LatestBlogs
-        blogs={blogs}
-        isLoading={isLoadingBlogs}
-        isError={isErrorBlogs}
-      />
-    </>
-  );
+  return null;
 }
