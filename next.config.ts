af0770944bdbae1ba226824d81e234e1ce787a99
MinhDat1312 <nguyenthangdat84@gmail.com>
2026-01-20 17:24:41 +0700

    fix: allow S3 image domain in next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,53 +1,59 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "avatars.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'avatars.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "boringapi.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'goat-s3-bucket-storage.s3.ap-southeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "api.dicebear.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'boringapi.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "t2.gstatic.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 't2.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
-  allowedDevOrigins: ["192.168.0.245"],
+  allowedDevOrigins: ['192.168.0.245'],
 };
 
 export default nextConfig;
