b8c1f38bd971bdc1fdd876dd7109305f961e9004
trgcghai <conghai.tpma@gmail.com>
2026-01-20 07:52:00 +0700

    fix: remove debug console log from layout component

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -14,8 +14,6 @@ export default function ChatLayout({ children }: ChatLayoutProps) {
 
   useSubscribeToMessagesQuery(undefined, { skip: !isSignedIn });
 
-  console.log("Test");
-
   return (
     <div className="h-screen flex overflow-hidden">
       <div className="w-full md:w-[450px] shrink-0 border-r border-border">
