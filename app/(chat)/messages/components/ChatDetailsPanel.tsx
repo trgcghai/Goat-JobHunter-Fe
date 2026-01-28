1b0a6bcd7bcd4aaa620d94c9b260ff1c09722edf
trgcghai <conghai.tpma@gmail.com>
2026-01-28 10:53:43 +0700

    refactor: remove unnecessary client directive from ChatDetailsPanel component

--- app/(chat)/messages/components/ChatDetailsPanel.tsx
+++ app/(chat)/messages/components/ChatDetailsPanel.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
